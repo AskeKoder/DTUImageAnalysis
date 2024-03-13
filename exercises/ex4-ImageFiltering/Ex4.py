f5b042f113c5200022e5bd1b5dab925a201cdc3b
Aske <askebanke@gmail.com>
2024-03-13 12:00:08 +0100

    wupw uow

--- exercises/ex4-ImageFiltering/Ex4.py
+++ exercises/ex4-ImageFiltering/Ex4.py
@@ -1,5 +1,6 @@
 #%%
 from scipy.ndimage import correlate
+from skimage import color
 import numpy as np
 
 #create small image 
@@ -39,11 +40,12 @@ from skimage import io
 import os
 os.chdir(r"C:\Users\askeb\OneDrive - Danmarks Tekniske Universitet\DTU\9. Semester\Image analysis\Scripts og data\DTUImageAnalysis\exercises\ex4-ImageFiltering")
 im_org = io.imread('data/Gaussian.png')
+im_org = color.rgb2gray(im_org)
 io.imshow(im_org)
 
 #%%
 #Create weight matrix for mean filtering
-size = 5
+size = 10
 # Two dimensional filter filled with 1
 weights = np.ones([size, size])
 # Normalize weights
@@ -51,7 +53,43 @@ weights = weights / np.sum(weights)
 print(weights)
 #%%
 #Apply filter
-im_filter = correlate(im_org, weights)
+mean_im = correlate(im_org, weights)
+io.imshow(mean_im)
+# Image becomes blurry and more so when filter size is increased
+
+# %% Median filtering =================================
+from skimage.filters import median
+
+size = 10
+footprint = np.ones([size, size])
+med_im = median(im_org, footprint)
+io.imshow(med_im)
+
+#Noise is removed and edges are preserved 
+#better than with mean filtering
+# %% Test on salt and pepper img
+from skimage import img_as_ubyte
+im_org = io.imread('data/SaltPepper.png')
+print(im_org.shape)
+im_gray = img_as_ubyte(color.rgb2gray(im_org))
+io.imshow(im_gray)
+
+
+import matplotlib.pyplot as plt
+fig, axs=plt.subplots(1,2)
+size = 5
+#Apply mean filter
+weights = np.ones([size, size])
+weights = weights / np.sum(weights)
+mean_im = correlate(im_gray, weights)
+io.imshow(mean_im, ax=axs[0])
+
+#Apply median filter
+footprint = np.ones([size, size])
+med_im = median(im_gray, footprint)
+io.imshow(med_im)
+med_im = median(im_gray, footprint)
+io.imshow(med_im, ax=axs[1])
 
 
 # %%
